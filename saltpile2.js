85743f197c30e9d057db495d5d0795d71ff61f7b
Eric White <led.ecodyswhite@gmail.com>
2023-11-24 13:35:28 -0800

    silly whoopsie

--- saltpile2.js
+++ saltpile2.js
@@ -3,12 +3,6 @@ let canvas = document.getElementById("snowCanvas");
 setHeightWidth(); //initial height and width of canvas;
 window.onresize = setHeightWidth; //Reset W and H values when changing windows
 
-// red = imgData.data[0];
-// green = imgData.data[1];
-// blue = imgData.data[2];
-// alpha = imgData.data[3];
-//colors legend
-
 function setHeightWidth() {
   W = window.innerWidth;
   H = window.innerHeight;
@@ -17,48 +11,49 @@ function setHeightWidth() {
 }
 canvas.style.backgroundColor = "black"; //set background color
 
-//draw a single pixel on a random x value at y = 0
-//okay now draw it at a random x position
-//okay now write a function to check the color of the pixel
-//you need to look at the four color values, rgba:
-//realistically you only need to confirm that the first indice != 0
-//check the pixel to the right
-//check the pixel to the left
-//handle for out of bounds
-//add to the left or right, or go up a level.
-//for reference, it's if colorData2 != 0
-//or i suppose in the future case, colorData != 0
-//write a function to put a white pixel on the canvas, if one isn't already present
-//let's write a helper function to check the color.
-//does it need a helper function?
-
 //eat your pea professor
 
 const ctx = canvas.getContext("2d");
 
 ctx.fillStyle = "#FFFFFF";
-ctx.fillRect(Math.floor(Math.random() * W - 1) - 10, H - 10, 10, 10);
 // ctx.fillRect(W - 100, H - 100, 100, 100)
-// let colorData2 = ctx.getImageData(W - 100, H - 100, 1, 1).data[0]
-// let colorData = ctx.getImageData(Math.floor(Math.random() * W - 1) -10, H - 10, 10, 10).data[0];
-// console.log(colorData2)
+// let colorData2 = ctx.getImageData(W - 100, H - 100, 1, 1).data
+// console.log(`r:${colorData2[0]} g:${colorData2[1]} b:${colorData2[2]} a:${colorData2[3]}`)
+// preserve that
+
+//so if any one of those values != 0
+//or if [0] != 0
+//make it that color.
+//else check to the right
+//else check to the left
+//else go up one level and repeat
 
 //alright, now let's put this in a function that can be called on a timer
 
 //let's add a dummy value to the array, and write logic to test for dummy value
 
+let dummyWidth = 10
 
 function putSalt() {
-  let y = 0; //you'll want to reinitialize y as zero each iteration
-  let randomX = Math.floor(Math.random() * dummyWidth);
-  console.log(`x: ${randomX} y: ${y}`)
-  if (saltMap[0][0] == randomX && saltMap[0][1] == y) {
-    console.log("Can't let you do that starfox")
-    if(outOfRange.includes(saltMap[0][0] - 1)){
-        //code for if low
-    }
-    // ctx.fillRect(randomX, y, 100, 100);
+  let y = H; //you'll want to reinitialize y as zero each iteration
+//wrong reinitialize it as H
+  let randomX = Math.floor(Math.random() * W);
+  colorData = ctx.getImageData(randomX, y, 1, 1).data
+  console.log(colorData[0])
+  if(colorData[0] == 0){
+    ctx.fillRect(randomX, y - 100, 10, 10)
+    console.log(colorData[0])
   }
+  console.log(`x: ${randomX} y: ${y}`)
+
+//sanity tests
+// ctx.fillRect(W - 100, H - 100, 100, 100)
+// let colorData2 = ctx.getImageData(W - 100, H - 100, 1, 1).data
+// console.log(colorData2[0])
+// console.log(ctx.getImageData(0,0,1,1).data[0])
+// ctx.fillRect(0,0,1,1)
+// console.log(ctx.getImageData(0,0,1,1).data[0])
+
 }
 
 putSalt();
@@ -81,3 +76,5 @@ putSalt();
 //or, if you hit the range when checking increment/decrement
 //just increment y; that way it can get a sticky buildup effect
 //on the sides
+
+//y does not need to equal zero; it needs to equal height.
