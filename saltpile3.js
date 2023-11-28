9bb9606fa52a974be3749522534a260e10c95b9d
Eric White <led.ecodyswhite@gmail.com>
2023-11-27 17:41:42 -0800

    polishing

--- saltpile3.js
+++ saltpile3.js
@@ -7,12 +7,10 @@
 
 window.onload = function () {
   let canvas = document.getElementById("snowCanvas");
-  setHeightWidth(); //initial height and width of canvas;
+  setHeightWidth(); //initialize height and width of canvas;
   window.onresize = setHeightWidth; //Reset W and H values when changing windows
 
   function setHeightWidth() {
-    // let W = window.innerWidth;
-    // let H = window.innerHeight;
     let W = window.innerWidth;
     let H = window.innerHeight;
     canvas.width = W;
@@ -20,17 +18,18 @@ window.onload = function () {
   }
 
   canvas.style.backgroundColor = "black"; //set background color
+  //this can be set to whatever is most convenient for the client to chroma key out
 
   const ctx = canvas.getContext("2d", { willReadFrequently: true });
 
-  ctx.fillStyle = "#FFFFFF";
+  ctx.fillStyle = "#FFFFFF"; //yummy salt color
 
 
   const y = window.innerHeight;
   const x = window.innerWidth;
   let cascArr = new Array(y);
 
-  //this for loop initializes the array
+  //this for loop initializes the arrays within the array
 
   for (var i = 0; i < y; i++) {
     cascArr[i] = new Array();
@@ -43,46 +42,25 @@ window.onload = function () {
     while (yCoord < y) {
       if (!arr[yCoord].includes(xCoord)) {
         arr[yCoord].push(xCoord);
-        // console.log(`pushing ${xCoord}, ${yCoord}`);
-        setTimeout( ctx.fillRect(xCoord, yCoord, 4, 4), 100 )
+        ctx.fillRect(xCoord, yCoord, 4, 4) //the last two arguments determine the size of salt
         yCoord = 799;
         return;
       } else if (!arr[yCoord].includes(xCoord - 1) && xCoord - 1 > -1) {
         arr[yCoord].push(xCoord - 1);
-        // console.log(`pushing ${xCoord} - 1, ${yCoord}`);
-        ctx.fillRect(xCoord - 1, yCoord, 4, 4)
+        ctx.fillRect(xCoord - 1, yCoord, 4, 4) //the last two arguments determine the size of salt
         yCoord = 799;
         return;
       } else if (!arr[yCoord].includes(xCoord + 1) && xCoord + 1 < x) {
         arr[yCoord].push(xCoord + 1);
-        // console.log(`pushing ${xCoord} + 1, ${yCoord}`);
-        ctx.fillRect(xCoord + 1, yCoord, 4, 4)
+        ctx.fillRect(xCoord + 1, yCoord, 4, 4) //the last two arguments determine the size of salt
         yCoord = 799;
         return;
       } else {
         yCoord -= 1;
-        // console.log("incrementing Y");
       }
     }
   }
 
-  // for (let i = 0; i < 100000; i++) {
-  //   //this is the loop condition that matters.
-  //   cascadeArray(cascArr, x, y); //the number, currently 2500, should be dependent
-  //   //on the width times length of the canvas.
-  // }
-  // console.log(cascArr);
-
-  setInterval(function(){cascadeArray(cascArr, x, y);}, 100);
-
-
-  // the arrays behave as they should
-  // now to build the canvas
-  // let's start with a test 50x50 canvas.
-
-  // the settimeout is being finnicky
-  //maybe if i put settimeout in each pixel placement
-
-  //referencing a super old color clicker thing i made to put the pixels properly
-  
+  setInterval(function(){cascadeArray(cascArr, x, y);}, 100); //will run every 100 miliseconds
+  //value can be changed to client's tastes
 };
